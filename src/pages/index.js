b607a83f4cf4e9df2ddf1aab226e4b99d31dbaee
Verd1ne <76729013+Verd1ne@users.noreply.github.com>
2022-07-25 21:03:49 +0700

    Environment

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import * as React from "react"
+
+const IndexPage = () => {
+  return (
+    <main>
+      <title>Home Page</title>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </main>
+  )
+}
+
+export default IndexPage
