b13df5e6b72a35aba64d208dbe0faf669117f277
fhuzi <abddheuijd@gmail.com>
2022-08-13 15:51:59 +0700

    yellow hover
    s

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,12 +12,12 @@ export default function Header() {
         layout="fixed"
         height= {64}
       />
-      <div className="flex-row my-auto text-white space-x-7 font-ProductSans text-2xl">
-        <Link to="/">About</Link>
-        <Link to="/">Store</Link>
-        <Link to="/">Competition</Link>
-        <Link to="/">Registration</Link>
-        <Link to="/">Soundscape</Link>
+      <div className="flex-row my-auto text-white space-x-7 font-ProductSans text-2xl lg:p-4 py-3 px-0 block border-b-2 border-transparent font-bold">
+        <Link className="hover:text-yellow-500" to="/">About</Link>
+        <Link className="hover:text-yellow-500" to="/">Competition</Link>
+        <Link className="hover:text-yellow-500" to="/">Store</Link>
+        <Link className="hover:text-yellow-500" to="/">Soundscape</Link>
+        <Link className="hover:text-yellow-500" to="/">Registration</Link>
       </div>
     </div>
   )
