e1cc01d6d4b2b3abebfd5adce61e1ddaab0afa09
deniseLie <76108238+deniseLie@users.noreply.github.com>
2022-09-23 23:04:43 +0700

    homepage + footer + header done(?)

--- src/components/HomePage/Themesong.js
+++ src/components/HomePage/Themesong.js
@@ -1,14 +1,20 @@
 import React from 'react'
 import { FaSpotify } from "@react-icons/all-files/fa/FaSpotify";
-import { AiOutlineYoutube } from "@react-icons/all-files/ai/AiOutlineYoutube";
+import { AiFillYoutube } from "@react-icons/all-files/ai/AiFillYoutube";
 
 export default function Themesong() {
   return (
     <div className="relative z-10 bg-[url('../images/Disc.png')] bg-no-repeat bg-top bg-contain px-12 h-[300px] md:h-[600px] lg:h-[700px]">
-      <div className="text-center text-white font-Broadway text-[30px] md:text-[80px] lg:text-[100px] mt-12 overflow-hidden mb-3 md:mb-6">THEME SONG</div>
-      <div className="flex flex-row text-center gap-4 place-content-center">
-        <a href="https://www.instagram.com/smukiee/" target="blank" className='text-red-900 font-bold text-xs md:text-xl lg:text-2xl bg-white sm:py-2 px-2 sm:px-4 shrink animate-pulse hover:bg-gray-400'><p className="inline mr-2">SPOTIFY</p><FaSpotify color="red" size={32} className="inline w-[20px] md:w-[30px] lg:w-[32px] overflow-fit"/></a>
-        <a href="https://www.instagram.com/smukiee/" target="blank" className='text-red-900 font-bold text-xs md:text-xl lg:text-2xl bg-white sm:py-2 px-2 sm:px-4 shrink animate-pulse hover:bg-gray-400'><p className="inline mr-2">YOUTUBE</p><AiOutlineYoutube color="red" size={32} className="inline w-[25px] md:w-[33px] overflow-fit"/></a>
+      <div className="text-center text-white font-Broadway text-[30px] md:text-[80px] lg:text-[100px] mt-3 overflow-hidden mb-1">THEME SONG</div>
+      <div className="flex flex-row text-center gap-4 md:gap-16 place-content-center">
+        <a href="https://www.instagram.com/smukiee/" target="blank" className='text-red-600 font-extrabold text-sm md:text-3xl lg:text-4xl bg-white pt-0.5 sm:py-2 px-2 sm:px-4 animate-pulse hover:bg-gray-400'>
+          <div className="inline mr-1 md:mr-3">SPOTIFY</div>
+          <FaSpotify color="red" size={32} className="inline w-[13px] md:w-[30px] lg:w-[35px] -mt-0.5 md:-mt-1.5 h-fit overflow-fit"/>
+        </a>
+        <a href="https://www.instagram.com/smukiee/" target="blank" className='text-red-600 font-extrabold text-sm md:text-3xl lg:text-4xl bg-white pt-0.5 sm:py-2 px-2 sm:px-4 animate-pulse hover:bg-gray-400'>
+          <p className="inline mr-1 md:mr-3">YOUTUBE</p>
+          <AiFillYoutube color="red" size={32} className="inline w-[20px] md:w-[35px] lg:w-[45px] -mt-0.5 md:-mt-1.5 h-fit overflow-fit"/>
+        </a>
       </div>
     </div>
   )
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import * as React from "react"
 import { StaticImage } from "gatsby-plugin-image"
 import Layout from "../components/Layout/Layout"
 import Nowshowingcard from "../components/HomePage/Nowshowingcard"
+import Preview from "../components/HomePage/Preview"
 import Themesong from "../components/HomePage/Themesong"
 import Timeline from "../components/HomePage/Timeline"
 import Sponsors from "../components/HomePage/Sponsors"
@@ -18,7 +19,7 @@ const IndexPage = () => {
         className="mt-16"
         width={1920}
       />
-      <div className="text-[#f5f1be] font-Palatino text-center text-sm md:text-lg lg:text-2xl mx-12">
+      <div className="text-[#f5f1be] font-Palatino text-center text-xs md:text-lg lg:text-2xl mx-5 md:mx-12">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut eu nunc eget ex viverra condimentum. Integer suscipit enim nibh, sit amet ornare ligula tincidunt in. Mauris accumsan ex nec condimentum lacinia. Donec a feugiat metus, vel venenatis purus. Aenean elementum odio et lorem cursus consequat eu eget tortor. Etiam sit amet tempus neque. Aliquam in metus tellus. Aenean elementum ligula vitae facilisis venenatis. Nulla scelerisque scelerisque nisl nec mollis. Vestibulum ultrices id metus quis dapibus. Nulla facilisi. Fusce volutpat lectus et rutrum mollis. Quisque non suscipit augue. In est felis, convallis et sodales non, eleifend vel odio. In et aliquet.
       </div>
       <StaticImage 
@@ -27,17 +28,16 @@ const IndexPage = () => {
         placeholder="blurred"
         width={1920}
       />
-      <div className="relative grid grid-cols-3 mx-5 md:mx-10 lg:mx-12 lg:px-20 mb-3">
+      <div className="relative grid grid-cols-3 mx-5 md:mx-10 lg:mx-20 lg:px-20">
         <Nowshowingcard/>
         <Nowshowingcard/>
         <Nowshowingcard/>
       </div>
-      <div className="text-white text-center mt-2">
+      <div className="text-[10px] md:text-2xl xl:text-3xl text-white text-center md:mt-3 lg:mt-0">
         View More
         <BsArrowRight className="inline" size={24}/>
       </div>
-      <div className="text-center text-white font-Broadway text-[50px] md:text-[90px] lg:text-[110px] mt-12 overflow-hidden md:mb-6">PREVIEW</div>
-      <div className="text-[#f5f1be] font-Palatino text-[30px] md:text-[50px] lg:text-[60px] text-center overflow-hidden mb-6">our opening video</div>
+      <Preview/>
       <Themesong/>
       <Timeline/>
       <Sponsors/>
