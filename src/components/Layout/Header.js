aa167b32a3c53a64378baa17b354c62fedb9e336
Verd1ne <76729013+Verd1ne@users.noreply.github.com>
2022-09-27 11:11:09 +0700

    Header fix and Themesong button animation

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -7,57 +7,55 @@ export default function Header() {
   const [dropdown, setDropdown] = useState(false);
 
   return (
-    <div className="fixed z-[100] w-full h-fit">
-      <div className="grid grid-cols-3 md:grid-cols-4 bg-black flex justify-between pr-2 md:pr-5 lg:pr-10 pt-3 py-2 xl:pb-5 w-screen h-fit max-h-min">
-        <div className="md:span-cols-1 w-fit h-fit">
-          <Link to="/" className="">
+    <div className="fixed z-[100] w-full">
+      <div className="grid grid-cols-4 sm:grid-cols-3 lg:grid-cols-4 bg-black flex justify-between w-screen h-16 pr-4 md:pr-8 lg:pr-12">
+          <Link to="/" className="col-span-3 sm:col-span-2 lg:col-span-1">
             <StaticImage 
               src= "../../images/SmakonecupLogo.png"
               alt= "logo"
               placeholder="blurred"
-              // layout="fixed"
-              // height= {64}
-              className="h-[20px] md:h-[40px] lg:h-[64px] md:w-5/6 lg:w-5/6 xl:w-5/6 xl:mt-2"
+              layout="fixed"
+              height= {64}
+              className=""
             />
           </Link>
-        </div>
-        <div className="col-start-3 md:col-start-2 md:col-span-3">
+        <div className="col-start-4 sm:col-start-3 lg:col-span-3">
           {/* laptop */}
-          <div className="hidden md:flex flex-row my-auto text-white md:space-x-4 lg:space-x-7 font-ProductSans md:text-lg lg:text-2xl xl:text-3xl w-full justify-end mt-1 lg:mt-4 xl:mt-6">
-            <Link to="/" className="headerButton h-full">About</Link>
+          <div className="hidden lg:flex text-white space-x-4 lg:space-x-7 font-ProductSans text-xl lg:text-2xl w-full justify-end my-auto h-full items-center inline">
+            <Link to="/" className="headerButton">About</Link>
             <Link to="/" className="headerButton">Store</Link>
             <Link to="/scoreboard" className="headerButton">Competition</Link>
             <Link to="/" className="headerButton">Registration</Link>
             <Link to="/" className="headerButton">Soundscape</Link>
           </div>
           {/* mobile */}
-          <div className="flex md:hidden justify-end align-center">
+          <div className="flex lg:hidden justify-end items-center h-full">
             <button onClick={() => setDropdown(prev => !prev)}>
-              <GiHamburgerMenu color="white" size={20} />
+              <GiHamburgerMenu color="white" size={40} />
             </button>
           </div>
         </div>
       </div>
-      <div className={`${dropdown ? "" : "hidden"} w-fit fixed right-0 px-2 pt-1 -mt-2 rounded divide-y bg-black`}>
+      <div className={`${dropdown ? "" : "hidden"} lg:hidden w-fit fixed right-0 px-2 pt-1 -mt-2 rounded divide-y bg-black`}>
         <ul className="py-1 text-[10px] text-gray-700 dark:text-gray-200 text-right">
           <li>
-            <Link to="/" className="headerButton h-full">About</Link>
+            <Link to="/" className="headerButton headerButtonThin">About</Link>
           </li>
           <li>
-            <Link to="/" className="headerButton">Store</Link>
+            <Link to="/" className="headerButton headerButtonThin">Store</Link>
           </li>
           <li>
-            <Link to="/scoreboard" className="headerButton">Competition</Link>
+            <Link to="/scoreboard" className="headerButton headerButtonThin">Competition</Link>
           </li>
           <li>
-            <Link to="/" className="headerButton">Registration</Link>
+            <Link to="/" className="headerButton headerButtonThin">Registration</Link>
           </li>
           <li>
-            <Link to="/" className="headerButton">Soundscape</Link>
+            <Link to="/" className="headerButton headerButtonThin">Soundscape</Link>
           </li>
             {/* <a href="#" className="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Earnings</a> */}
         </ul>
+      </div>
     </div>
-  </div>
   )
 }
