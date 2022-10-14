f288712beacaccb7a78236bef82167785e3125f1
Klein-Edbert <87744095+Klein-Edbert@users.noreply.github.com>
2022-10-14 20:37:01 +0700

    nowshowing responsive

--- src/components/HomePage/Nowshowingcard.js
+++ src/components/HomePage/Nowshowingcard.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Nowshowingcard(props) {
   return (
-    <div className="m-8 lg:m-0">
+    <div className="m-4 lg:m-0">
       <img src={props.image} className="lg:p-4 xl:p-8"/>
     </div>
 
