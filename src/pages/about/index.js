44976f241d5ce480cd4be6807cb09c092363a062
Verd1ne <76729013+Verd1ne@users.noreply.github.com>
2022-10-23 21:46:49 +0700

    About page fix

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -7,37 +7,58 @@ import "@splidejs/splide/dist/css/themes/splide-default.min.css";
 export default function Index() {
   return (
     <Layout>
-      <div className="text-white overflow-x-hidden mx-[35px] md:mx-[80px] lg:mx-[125px] xl:mx-[300px] mb-[50px] md:mb-[60px] lg:mb-[80px] xl:mb-[100px]">
-        <div className='font-Broadway text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl overflow-hidden mt-32 mb-[50px] md:mb-[70px] lg:mb-[80px] xl:mb-[120px] text-center text-white'>About SOC</div>
-        <div className="border border-white border-2 rounded-3xl px-5 md:px-10 lg:px-12 xl:px-15 py-5 md:py-7 lg:py-10 xl:py-12 mb-[50px] md:mb-[60px] lg:mb-[80px] xl:mb-[100px]">
-          <p className='text-white text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl font-bold mb-[15px] lg:mb-[30px] 2xl:mb-[40px] text-center overflow-hidden'>Apa itu SOC (SMAKONECUP)</p>
-          <div className="text-justify text-sm md:text-base lg:text-lg xl:text-xl ">
+      <div className="text-white overflow-x-hidden mx-[35px] md:mx-[80px] lg:mx-[125px] xl:mx-[150px] 2xl:mx-[275px] mb-[50px] md:mb-[60px] lg:mb-[80px] xl:mb-[100px]">
+        <div         
+        data-aos="fade-in"
+        data-aos-offset="0"
+        data-aos-duration="3000"
+        className='font-Broadway text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl overflow-hidden mb-[50px] md:mb-[70px] lg:mb-[80px] xl:mb-[120px] text-center text-white mt-36'>About SOC</div>
+        <div 
+        data-aos="zoom-in-up"
+        data-aos-offset="0"
+        data-aos-duration="1500"
+        className="border border-white border-2 rounded-3xl px-5 md:px-10 lg:px-12 xl:px-15 py-5 md:py-7 lg:py-10 xl:py-12 mb-[50px] md:mb-[60px] lg:mb-[80px] xl:mb-[100px]">
+          <p className='text-white text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl font-bold mb-[15px] lg:mb-[30px] 2xl:mb-[40px] text-center overflow-hidden font-ProductSans'>Apa itu SOC (SMAKONECUP)</p>
+          <div className="text-justify text-md md:text-lg lg:text-xl xl:text-2xl font-OpenSans">
               <div>SMAKONECUP 2022 merupakan salah satu acara terbesar dalam serangkaian program satu tahun kami. Acara ini merupakan event persahabatan antar sekolah yang memperlombakan 16 cabang lomba baik dalam bidang akademis maupun non akademis.</div>
               <br/>
               <div>Dengan diadakannya SMAKONECUP 2022, diharapkan tujuan pengembangan potensi generasi muda secara menyeluruh dapat tercapai. Generasi muda dapat mengembangkan secara luas dan mendalam kemampuan dan potensi yang dimiliki dan mengekspresikannya melalui acara ini, terlepas dari kemampuan peserta didik dalam akademis namun juga menunjukan kemampuan peserta didik di berbagai bidang kreasi lainnya.</div>
           </div>
         </div>
-        <div className="lg:grid lg:grid-cols-3 lg:gap-x-5 xl:mx-[24px] 2xl:mx-[40px]">
-          <div className="lg:col-span-2 mb-[50px] md:mb-[60px] lg:mb-[80px] xl:mb-[100px]">
-            <div className='border border-2 border-white  rounded-3xl px-5 md:px-10 lg:px-12 xl:px-15 py-5 md:py-7 lg:py-10 xl:py-12 w-fit'>
-              <p className='text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl font-bold mb-[15px] lg:mb-[30px] 2xl:mb-[40px] text-center overflow-hidden'>Tema SOC : ATRAXIA</p>
-              <div className="text-justify text-sm md:text-base lg:text-lg xl:text-xl">
+        <div className="lg:grid lg:grid-cols-3 lg:gap-x-5 xl:mx-[24px] 2xl:mx-[0px] mb-[50px] md:mb-[60px] lg:mb-[80px] xl:mb-[100px] mt-36 md:mt-44 lg:mt-52 xl:mt-60">
+          <div 
+            data-aos="zoom-in-right"
+            data-aos-offset="0"
+            data-aos-duration="1500"
+            data-aos-delay="1000"
+            className="lg:col-span-2 flex">
+            <div className='border border-2 border-white rounded-3xl px-5 md:px-10 lg:px-12 xl:px-15 py-5 md:py-7 lg:py-10 xl:py-12 w-fit '>
+              <p className='text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl font-bold mb-[15px] lg:mb-[30px] 2xl:mb-[40px] text-center overflow-hidden font-ProductSans'>Tema SOC : ATRAXIA</p>
+              <div className="text-justify text-md md:text-lg lg:text-xl xl:text-2xl font-OpenSans">
                   ATRAXIA yang merupakan singkatan dari “Above The Rest; eXpress your Inner personA”. Tema tahun ini terinspirasi dari keinginan kami agar setiap individu memiliki kesempatan untuk mengekspresikan dan mengeksplor diri masing-masing sehingga bisa menghasilkan inovasi-inovasi baru tanpa memiliki rasa kekhawatiran akan kegagalan. Hal ini bertujuan agar kita berani untuk menerobos rintangan dan menjadi berbeda dalam menunjukan warna kita yang sebenarnya dan agar kita bisa membawa perubahan positif ke depannya seiring dengan maraknya kemajuan teknologi dan banyaknya perubahan di era globalisasi ini dengan tetap memegang teguh prinsip-prinsip baik yang telah tertanam dan mempertahankan jati diri kita sebagai anak bangsa.
               </div>
             </div>
           </div>
-          <div className="hidden lg:flex">
+          <div 
+          data-aos="zoom-in-left"
+          data-aos-offset="0"
+          data-aos-duration="1500"
+          className="hidden lg:flex w-full">
             <StaticImage 
                 src= "../../images/banner_atraxia.webp"
                 alt= "logo"
                 placeholder=""
-                layout="constrained"
-                // height={500}
-                className="lg:h-[610px] xl:h-[690px] 2xl:h-[720px]"
+                layout="fixed"
+                height={600}
+                className="mr-0 ml-auto my-auto"
               />
           </div>
         </div>
-        <p className='text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl text-white font-bold mb-[30px] lg:mb-[30px] 2xl:mb-[40px] text-center md:h-12 lg:h-20'>SOC Throughout The Years</p>
+        <p 
+        data-aos="fade-in"
+        data-aos-offset="400"
+        data-aos-duration="1500"
+        className='text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl text-white font-bold mb-[30px] lg:mb-[30px] 2xl:mb-[40px] text-center md:h-12 lg:h-20 font-ProductSans  mt-36 md:mt-44 lg:mt-52 xl:mt-60'>SOC Throughout The Years</p>
         <Splide
           options={{
             rewind: true,
