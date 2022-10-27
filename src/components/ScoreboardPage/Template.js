a10c15326d234ba191a91cd842f8c5a672f435f3
RReven <jasonkeith0311@gmail.com>
2022-10-27 14:21:35 +0700

    push untuk lfr

--- src/components/ScoreboardPage/Template.js
+++ src/components/ScoreboardPage/Template.js
@@ -1,12 +1,40 @@
+import { useState, useEffect } from 'react'
 import * as React from "react"
 import Infolomba from './Infolomba'
 import Timeline from "./Timeline"
 import Hadiah from "./Hadiah"
+import Scoreboard from "./Scoreboard"
 import AOS from 'aos';
 import 'aos/dist/aos.css'; 
 AOS.init();
 
 export default function Template( props ) {
+  const [datas, setDatas] = useState([])
+  const sheetID = '1pk-6GLpbx3yRb52osPydVpdsesJbtXE-Mf_VgX5irMA'
+  const key = 'AIzaSyCyeHkUFZ5kWAwhCQZAm_hrOWPDUNfx_Lw'
+  const endpoint = `https://sheets.googleapis.com/v4/spreadsheets/${sheetID}?key=${key}`;
+
+  const getData = () => {
+    fetch(endpoint).then(response => response.json()).then(data => {
+      data?.sheets.forEach(s => {
+        const sheetName = s.properties.title;
+        const API = `https://sheets.googleapis.com/v4/spreadsheets/${sheetID}/values:batchGet?ranges=${sheetName}&valueRenderOption=ROWS&key=${key}`;
+        fetch(API).then(response => response.json()).then(data => {
+          let rows = data.valueRanges[0].values;
+
+          setDatas(rows)
+        });
+      });
+    });
+  }
+    
+  useEffect(() => {
+    setTimeout(() => {
+      console.log(datas)
+      getData();
+    }, 5000)
+    }, [datas]);
+
   return (
       <div className='grid grid-cols-1 xl:grid-cols-2 relative mt-32 mb-16 md:mb-20 lg:mb-24 xl:mx-16 2xl:mx-64'>
         {/* <div data-aos="fade-in"
@@ -17,6 +45,13 @@ export default function Template( props ) {
             className="mx-auto mt-12 col-span-1 xl:col-span-5">
           <Hadiah/>
         </div> */}
+        <div data-aos="fade-in"
+            data-aos-duration="3000"
+            data-aos-once="true"
+            data-aos-anchor-placement="top-bottom"
+            className="mx-auto mt-12 col-span-1">
+          <Scoreboard timA={ props.timA } timB={ props.timB } skorA={ props.skorA } skorB={ props.skorB }/>
+        </div>
         <div data-aos="fade-in"
             data-aos-duration="3000"
             data-aos-once="true"
