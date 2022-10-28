5ce12d51bb3e44376752b741c5a6cf63d79b1835
Klein-Edbert <87744095+Klein-Edbert@users.noreply.github.com>
2022-10-28 13:48:02 +0700

    c

--- src/pages/scoreboard/index.js
+++ src/pages/scoreboard/index.js
@@ -10,12 +10,12 @@ export default function Scoreboard( props ) {
         <Layout>
             <SplideCompe/>
             <div className='font-Broadway text-5xl text-[#f5f1be] my-10 overflow-hidden text-center'>SCOREBOARD BASKET</div>
-            <div className="text-[#f5f1be] md:text-xl lg:text-2xl font-semibold font-ProductSans text-center md:mt-[70px] px-10 md:px-24 mx-11 md:mx-0">
+            <div className="text-[#f5f1be] md:text-xl lg:text-2xl font-semibold font-ProductSans text-center md:mt-[70px] px-10 md:mx-0">
                 <div className='text-white text-3xl lg:text-5xl mt-24 mb-8 text-left'>DAY 1<hr className="mt-3"/></div>
                 <Score
-                    team1 = "SMAK 1 PENABUR"
+                    team1 = "SMAK 1 PENABUR C (PUTRA)"
                     score1 = "LOSE"
-                    team2 = "SMAK GADING SERPONG"
+                    team2 = "SMAK GADING SERPONG (PUTRA)"
                     score2 = "WIN"
                 />
                 <Score
