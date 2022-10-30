06d17b36f6a29f5b284f98a8b74e9344ee30dfb1
deniseLie <76108238+deniseLie@users.noreply.github.com>
2022-10-30 15:23:13 +0700

    uwaw

--- src/components/ScoreboardPage/Score.js
+++ src/components/ScoreboardPage/Score.js
@@ -30,12 +30,12 @@ export default function Score(props){
     }
 
     return(
-            <div className='grid grid-cols-3 gap-x-5 my-12 bg-red-900 border-2 border-gold rounded-3xl p-5 text-center align-center px-auto'>
+            <div className='grid grid-cols-3 gap-x-5 my-12 bg-red-900 border-2 border-gold rounded-3xl p-3 md:p-5 text-center align-center px-auto'>
                 <div className="font-bold rounded-3xl lg:p-10 justify-center mx-auto">
-                    <div className="bg-gray-900 text-[7px] xl:text-3xl md:text-lg xl:rounded-2xl rounded-lg xl:p-3 p-2 grid place-items-center py-auto xl:h-[95px] h-[70px] w-fit">{props.team1}</div>
-                    <div className="xl:text-7xl xl:mt-12 mt-3">{props.score1}</div>
+                    <div className="bg-gray-900 text-[10px] xl:text-3xl md:text-lg xl:rounded-2xl rounded-lg p-1 md:p-2 lg:p-3 grid place-items-center py-auto xl:h-[95px] h-[70px] lg:h-[90px] xl:h-[110px] w-fit">{props.team1}</div>
+                    <div className="md:text-3xl lg:text-5xl xl:text-7xl xl:mt-12 mt-3">{props.score1}</div>
                 </div>
-                <div className="lg:text-9xl text-3xl font-bold rounded-3xl align-middle my-auto">VS <br/> 
+                <div className="text-3xl lg:text-7xl font-bold rounded-3xl align-middle my-auto">VS <br/> 
             <button
             type="button"
             onClick={openModal}
@@ -175,8 +175,8 @@ export default function Score(props){
                     </div>
                 </div> */}
                 <div className="font-bold rounded-3xl lg:p-10 justify-center mx-auto">
-                    <div className="bg-gray-900 text-[7px] xl:text-3xl md:text-lg xl:rounded-2xl rounded-lg xl:p-3 p-2 grid place-items-center py-auto xl:h-[95px] h-[70px] w-fit">{props.team2}</div>
-                    <div className="xl:text-7xl xl:mt-12 mt-3">{props.score2}</div>
+                    <div className="bg-gray-900 text-[10px] xl:text-3xl md:text-lg xl:rounded-2xl rounded-lg p-1 md:p-2 lg:p-3 grid place-items-center py-auto xl:h-[95px] h-[70px] lg:h-[90px] xl:h-[110px] w-fit">{props.team2}</div>
+                    <div className="md:text-3xl lg:text-5xl xl:text-7xl xl:mt-12 mt-3">{props.score2}</div>
                 </div>         
             </div>      
     )
